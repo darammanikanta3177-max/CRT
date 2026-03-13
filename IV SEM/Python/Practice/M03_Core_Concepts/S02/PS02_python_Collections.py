f37e2bfce88f52b063f034b26b2df5936f0130f1
darammanikanta3177-max <darammanikanta3177@gmail.com>
2026-03-13 11:27:49 +0530

    mes2

--- IV SEM/Python/Practice/M03_Core_Concepts/S02/PS02_python_Collections.py
+++ IV SEM/Python/Practice/M03_Core_Concepts/S02/PS02_python_Collections.py
@@ -11,10 +11,23 @@ a = [1,2,3]
 a.append(4)
 a.insert(1,7)
 print(a)
-'''
+
 a = [(1,2,3,4,5)]
 print(a)
 a.remove(2)
 print(a)
 a.pop()
 print(a)
+
+a = set([1,2,3,4,5])
+a.remove(4)
+print(a)
+
+a = {1,2,3,4,5}
+b = {6,7,8,9,10}
+print(a.union(b))
+print(a.intersection(b))
+print(a.difference(b))
+'''
+t1 = (1,2,3,4,5)
+del t1
